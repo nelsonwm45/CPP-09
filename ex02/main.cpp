01805efa60c769f9e0b506efd1bf512f01331f1f
nelsonwm45 <nelsonwm45@gmail.com>
2025-11-08 02:05:28 +0800

    Added some example test cases

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -93,8 +93,40 @@ void	printComparisonCount(int size, size_t vectorComparisons, size_t dequeCompar
 
 
 /*
-	./PmergeMe 11 2 17 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
+	./PmergeMe 3 5 9 7 4
+	./PmergeMe 11 2 17 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7  (21 numbers) article
 	./PmergeMe `shuf -i 1-1000 -n 21 | tr "\n" " " `
+
+	Odd count (has straggler)
+	./PmergeMe 3 5 9 7 4
+
+	Already sorted
+	./PmergeMe 1 2 3 4 5 6 7 8
+
+	Reverse sorted
+	./PmergeMe 9 8 7 6 5 4 3 2 1
+
+	Many duplicates
+	./PmergeMe 5 5 5 5 5 5 5
+
+	Mixed with duplicates
+	./PmergeMe 10 1 10 2 10 3 10 4 10
+
+	Nearly sorted, one out of place
+	./PmergeMe 1 2 3 4 6 5 7 8 9 10
+
+	Small edge (single element)
+	./PmergeMe 42
+
+	Small edge (two elements)
+	./PmergeMe 2 1
+
+	Medium random (even length)
+	./PmergeMe 12 7 3 14 9 1 8 2 11 6 5 10
+
+	Larger mixed (odd length)
+	./PmergeMe 50 40 60 20 10 80 70 30 90 100 15 5 55
+
 */
 int main(int ac, char **av)
 {
